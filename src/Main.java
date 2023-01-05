8300f36e37aa4e3f7567776572ff4e7884b8d392
Pavel <lp.qroovemaster@gmail.com>
2023-01-05 23:59:52 +0300

    Homework - Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Defining variables
+
+        char charSymbol = 'x';
+
+        boolean myFlag = false;
+
+        byte byteNumber = 127;
+        short shortNumber = 32767;
+        int intNumber = 100500;
+        long longNumber = 42L;
+
+        float floatNumber = 1.333333333333333333333333333333333333333333333333333333333333333f;
+        double doubleNumber = 1.6666666666666666666666666666666666666666666666666666666666;
+
+        // Printing variables
+
+        System.out.println("This is char: " + charSymbol);
+        System.out.println("This is boolean: " + myFlag);
+        System.out.println("This is byte: " + byteNumber);
+        System.out.println("This is short: " + shortNumber);
+        System.out.println("This is int: " + intNumber);
+        System.out.println("This is long: " + longNumber);
+        System.out.println("This is float: " + floatNumber);
+        System.out.println("This is double: " + doubleNumber);
+
     }
 }
